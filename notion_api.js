955473a128c73ddf75bc78760a97443a2b574aff
Evan Bubniak <evan.bubniak@gmail.com>
2022-01-27 16:19:00 -0500

    Add option for writing a plan for the day

--- notion_api.js
+++ notion_api.js
@@ -29,9 +29,51 @@ async function getEntriesForDate(date) {
     }
 }
 
+async function getChildBlocks(blockId) {
+    const response = await notion.blocks.children.list({
+        block_id: blockId,
+    })
+    return response.results;
+}
+
+async function getNewDayTemplateBlocks() {
+    const templateId = "c890999bcc5548be9c4de4bb2ddbc804";
+    return await getChildBlocks(templateId);
+}
+
+async function updateLifeWikiDayStrategy(newText) {
+    const dayStrategyBlockId = "1299712f047c483c97bd28047b59efce";
+    updateBlockToText(dayStrategyBlockId, newText);
+}
+
+async function updateDayStrategy(dayEntryId, newText) {
+    const dayBlocks = await getChildBlocks(dayEntryId);
+    const strategyBlockId = dayBlocks[1].id;
+    updateBlockToText(strategyBlockId, newText)
+}
+
+async function updateBlockToText(blockId, newText) {
+    const response = await notion.blocks.update({
+        block_id: blockId,
+        callout: {
+            text: [{
+                type: "text",
+                text: {
+                    content: newText
+                }
+            }]
+        }
+    })
+    console.log(response);
+}
 
 async function createEntryForDate(date) {
     try {
+        const childBlocks = await getNewDayTemplateBlocks();
+        const strippedChildBlocks = childBlocks.map(childBlock => {
+            const {id, ...restOfBlock} = childBlock;
+            return restOfBlock;
+        })
         const response = await notion.pages.create({
             parent: { database_id: databaseId },
             properties: {
@@ -49,13 +91,40 @@ async function createEntryForDate(date) {
                         "start": dateToYYYYMMDD(date)
                     }
                 }
-            }
+            },
+            children: strippedChildBlocks
         })
         console.log(response);
         return response;
     } catch (error) {
         console.error(error.body);
     }
+}
+
+async function completeTasks(tasks, entryId) {
+    const propertiesObj = tasks.reduce((obj, task) => ({...obj, [task]: {checkbox: true}}), {});
+    const response = await notion.pages.update({
+        page_id: entryId,
+        properties: propertiesObj
+    });
+    console.log(response);
+}
+
+async function setSleepTime(sleepDate, wakeDate, entryId) {
+    
+    const dateObj = {
+        "start": getTimezoneFormattedDateStr(sleepDate),
+        "end": getTimezoneFormattedDateStr(wakeDate),
+    };
+    const response = await notion.pages.update({
+        page_id: entryId,
+        properties: {
+            "Time in Bed": {
+                "date": dateObj
+            }
+        }
+    });
+    console.log(response);
 }
 
 async function didWeightsOnDayBefore(date) {
@@ -86,34 +155,6 @@ async function didWeightsOnDayBefore(date) {
     }
 }
 
-async function completeTasks(tasks, entryId) {
-    console.log(tasks);
-    const propertiesObj = tasks.reduce((obj, task) => ({...obj, [task]: {checkbox: true}}), {});
-    console.log(propertiesObj);
-    const response = await notion.pages.update({
-        page_id: entryId,
-        properties: propertiesObj
-    });
-    console.log(response);
-}
-
-async function setSleepTime(sleepDate, wakeDate, entryId) {
-    
-    const dateObj = {
-        "start": getTimezoneFormattedDateStr(sleepDate),
-        "end": getTimezoneFormattedDateStr(wakeDate),
-    };
-    const response = await notion.pages.update({
-        page_id: entryId,
-        properties: {
-            "Time in Bed": {
-                "date": dateObj
-            }
-        }
-    });
-    console.log(response);
-}
-
 
 export {
     doesEntryHaveSleep,
@@ -121,5 +162,7 @@ export {
     createEntryForDate,
     didWeightsOnDayBefore,
     completeTasks,
-    setSleepTime
+    setSleepTime,
+    updateLifeWikiDayStrategy,
+    updateDayStrategy
 }
--- start_day.js
+++ start_day.js
@@ -1,21 +1,21 @@
 
-import { getEntriesForDate, createEntryForDate, completeTasks, setSleepTime, doesEntryHaveSleep } from './notion_api.js'
+import {updateDayStrategy, getEntriesForDate, createEntryForDate, completeTasks, setSleepTime, doesEntryHaveSleep, updateLifeWikiDayStrategy } from './notion_api.js'
 import { currentDateWithOffset, addTimezoneOffset, millisecsPerDay, millisecsPerHour } from './date_format.js';
+import { createInterface } from 'readline';
 import _yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 const yargs = _yargs(hideBin(process.argv));
 
-// fs = require("fs");
 const argv = yargs
     .option('date', {
         alias: 'd',
         demandOption: false,
-        describe: 'Date for configuration',
+        describe: 'Date for configuration (YYYY-MM-DD format)',
         type: 'string',
         default: '',
     })
     .option('sleep', {
-        alias: 's',
+        alias: ['s', 'b', 'bed'],
         demandOption: false,
         describe: 'sleep time',
         type: 'string',
@@ -35,15 +35,29 @@ const argv = yargs
         type: 'array',
         default: [],
     })
+    // .option('plan', {
+    //     alias: 'p',
+    //     demandOption: false,
+    //     describe: "Strategy for the day",
+    //     type: "string",
+    //     default: "",
+    // })
+    .option('plan', {
+        alias: 'p',
+        demandOption: false,
+        describe: "Set strategy for the day",
+        type: "boolean",
+        default: false
+    })
     .argv
 
-async function getTime(prompt) {
+async function getUserInput(prompt) {
     const readline = createInterface({
         input: process.stdin,
         output: process.stdout
     });
-    return new Promise(resolve => readline.question(prompt, time => {
-        resolve(time);
+    return new Promise(resolve => readline.question(prompt, userInput => {
+        resolve(userInput);
         readline.close();
     }))
 }
@@ -54,8 +68,8 @@ async function populateEntryWithSleep(bedtimeString, waketimeString, currDate, e
     let bedtimeWasYesterday;
     let waketime;
     bedtime = bedtimeString.substring(0, 4);
-    bedtimeWasYesterday = (bedtimeString.charAt(bedtimeString.length - 1) == "y");
     waketime = waketimeString.substring(0, 4);
+    bedtimeWasYesterday = (parseInt(bedtimeString) > parseInt(waketimeString));
     let sleepDate = bedtimeWasYesterday ? new Date(wakeDate.getTime() - millisecsPerDay) : new Date(wakeDate.getTime());
     sleepDate.setHours(parseInt(bedtime.substring(0, 2)), parseInt(bedtime.substring(2, 4)), 0, 0)
     wakeDate.setHours(parseInt(waketime.substring(0, 2)), parseInt(waketime.substring(2, 4)), 0, 0)
@@ -66,28 +80,28 @@ async function populateEntryWithSleep(bedtimeString, waketimeString, currDate, e
 
 
 
-const currDate = (argv.date === '') ? new Date(currentDateWithOffset) : addTimezoneOffset(new Date(argv.date));
+// const currDate = (argv.date === '') ? new Date(currentDateWithOffset) : addTimezoneOffset(new Date(argv.date));
+
+const currDate = (argv.date === '') ? new Date() : addTimezoneOffset(new Date(argv.date));
 const dayEntries = await getEntriesForDate(currDate);
 const dayEntry = (dayEntries.length != 0) ? dayEntries[0] : await createEntryForDate(currDate);
 
-if (argv.s !== '') {
+if (argv.sleep !== '') {
     populateEntryWithSleep(argv.sleep, argv.wake, currDate, dayEntry.id)
 } else {
     if (!await doesEntryHaveSleep(dayEntry.id)) {
-        const bedtimeString = await getTime('When did you go to bed? (format: HHMM, 24-hour time, append y to end if it was yesterday)');
-        const waketimeString = await getTime('when did you wake up?');
+        const bedtimeString = await getUserInput('When did you go to bed? (format: HHMM, 24-hour time, append y to end if it was yesterday)');
+        const waketimeString = await getUserInput('when did you wake up?');
         populateEntryWithSleep(bedtimeString, waketimeString, dayEntry.id);
     }
+}
+
+if (argv.plan) {
+    const plan = await getUserInput("What is your plan for the day?\n")
+    updateLifeWikiDayStrategy(plan);
+    updateDayStrategy(dayEntry.id, plan);
 }
 
 if (argv.tasksdone.length > 0) {
     completeTasks(argv.tasksdone, dayEntry.id);
 }
-
-// if (await didWeightsYesterday()) {
-//     console.log("no need to do weights today");
-// } else {
-//     console.log("do weights today")
-// }
-
-
