01e32e2c97318e621b1542d62c2858355dd10fe3
Evan Bubniak <evan.bubniak@gmail.com>
2022-04-22 17:19:26 -0400

    switch to echoing so it can be appended to either bashrc or zshrc

---
+++ echo_aliases.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+echo "alias notion=\"node $(pwd)/set_day.js\"" 
+echo "alias notion-project=\"node $(pwd)/set_project.js\"" 
+echo "alias notion-get=\"node $(pwd)/get_day.js\"" 
+echo "alias notion-new-project=\"node $(pwd)/new_project.js\"" 
--- set_aliases.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-echo "alias notion=\"node $(pwd)/set_day.js\"" >> ~/.bashrc
-echo "alias notion-project=\"node $(pwd)/set_project.js\"" >> ~/.bashrc
-echo "alias notion-get=\"node $(pwd)/get_day.js\"" >> ~/.bashrc
-echo "alias notion-new-project=\"node $(pwd)/new_project.js\"" >> ~/.bashrc
