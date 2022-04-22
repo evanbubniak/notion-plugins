3a3da788b5b4dc228d26807d6333351a7ed9220d
Evan Bubniak <evan.bubniak@gmail.com>
2022-04-22 11:01:39 -0400

    add bash script for adding program aliases

---
+++ set_aliases.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+echo "alias notion=\"node $(pwd)/set_day.js\"" >> ~/.bashrc
+echo "alias notion-get=\"node $(pwd)/get_day.js\"" >> ~/.bashrc
+echo "alias notion-new-project=\"node $(pwd)/new_project.js\"" >> ~/.bashrc
